c3e4f6084de246fcbb329bba0dc9b91edab54791
Dell <Khaldalsydmhmd5@gmail.com>
2023-02-01 06:23:30 -0800

    movies category

--- lib/screens/movies/category_item.dart
+++ lib/screens/movies/category_item.dart
@@ -8,8 +8,67 @@ import '../../models/Movies/categorymovies.dart';
 class categoryitem extends StatelessWidget {
   Genres Category  ;
   categoryitem( this.Category);
+  String image ='assets/images/action.jfif';
+
   @override
   Widget build(BuildContext context) {
+    if(Category.name=='Action'){
+      image='assets/images/action.jfif';
+    }
+    if(Category.name=='Adventure'){
+      image='assets/images/Adventure.jfif';
+    }
+    if(Category.name=='Animation'){
+      image='assets/images/Animation.jpeg';
+    }
+    if(Category.name=='Comedy'){
+      image='assets/images/Comedy.jfif';
+    }
+    if(Category.name=='Crime'){
+      image='assets/images/Crime.jfif';
+    }
+    if(Category.name=='Documentary'){
+      image='assets/images/Documentary.jfif';
+    }
+    if(Category.name=='Drama'){
+      image='assets/images/Drama.jpg';
+    }
+    if(Category.name=='Family'){
+      image='assets/images/Family.jfif';
+    }
+    if(Category.name=='Fantasy'){
+      image='assets/images/Fantasy.jpg';
+    }
+    if(Category.name=='History'){
+      image='assets/images/History.jfif';
+    }
+    if(Category.name=='Horror'){
+      image='assets/images/Horror.jfif';
+    }
+    if(Category.name=='Music'){
+      image='assets/images/Music.jfif';
+    }
+    if(Category.name=='Mystery'){
+      image='assets/images/Mystery.jfif';
+    }
+    if(Category.name=='Romance'){
+      image='assets/images/Romance.jfif';
+    }
+    if(Category.name=='Science Fiction'){
+      image='assets/images/Science Fiction.jfif';
+    }
+    if(Category.name=='TV Movie'){
+      image='assets/images/TV Movie.jfif';
+    }
+    if(Category.name=='Thriller'){
+      image='assets/images/Thriller.jfif';
+    }
+    if(Category.name=='War'){
+      image='assets/images/War.jfif';
+    }
+    if(Category.name=='Western'){
+      image='assets/images/Western.jfif';
+    }
     return InkWell(
       onTap: (){
         Navigator.pushNamed(context, categorylist.routeName,
@@ -18,7 +77,7 @@ class categoryitem extends StatelessWidget {
       child: Container(
         child: Stack(
           children: [
-            Image.asset('assets/images/action.jfif',fit: BoxFit.fill,),
+            Image.asset('$image',fit: BoxFit.fill,),
             Container(
               alignment:Alignment.topCenter,
               child: Text(Category.name??'',
--- lib/shared/styles/my_theme.dart
+++ lib/shared/styles/my_theme.dart
@@ -2,79 +2,82 @@ import 'package:flutter/material.dart';
 
 import 'colors.dart';
 
-class my_theme{
+class my_theme {
 
   static ThemeData lightmode = ThemeData(
     primaryColor: black,
-      colorScheme:ColorScheme(onError: Colors.white,
-          onSecondary: Colors.white,
-          error: Colors.red,
-          background: Colors.transparent,
-          onPrimary: Colors.white,
-          primary: Colors.black,
-          brightness: Brightness.light,
-          onBackground: black,
-          onSurface: Colors.black,
-          secondary: black,
-          surface: Colors.grey) ,
-      scaffoldBackgroundColor: black,
-      appBarTheme: AppBarTheme(
-        iconTheme: IconThemeData(
-          color: Colors.white,
-        ),
+    colorScheme: ColorScheme(onError: Colors.white,
+        onSecondary: Colors.white,
+        error: Colors.red,
+        background: Colors.transparent,
+        onPrimary: Colors.white,
+        primary: Colors.black,
+        brightness: Brightness.light,
+        onBackground: black,
+        onSurface: Colors.black,
+        secondary: black,
+        surface: Colors.grey),
+    scaffoldBackgroundColor: black,
+    appBarTheme: AppBarTheme(
+      iconTheme: IconThemeData(
         color: Colors.white,
       ),
-      textTheme: TextTheme(
-          headline1:TextStyle(
-            fontSize: 22,fontWeight: FontWeight.bold,color: Colors.white,
-          ),
-        subtitle1: TextStyle(fontSize: 14,fontWeight: FontWeight.bold,color: Colors.white
-        ),
-        subtitle2: TextStyle(fontSize: 14,fontWeight: FontWeight.bold,color: Colors.black
-        ),
+      color: Colors.white,
+    ),
+    textTheme: TextTheme(
+      headline1: TextStyle(
+        fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white,
       ),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor:Colors.transparent,
-        elevation: 0,
-        selectedItemColor: yallow,
-        unselectedItemColor: grey,
+      subtitle1: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white
       ),
+      subtitle2: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.bold, color: Colors.black
+      ),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      selectedItemColor: yallow,
+      unselectedItemColor: grey,
+    ),
   );
   static ThemeData darktmode = ThemeData(
-      primaryColor: Colors.white,
-      colorScheme:ColorScheme(onError: Colors.white,
-          onSecondary: black,
-          error: Colors.red,
-          background: Colors.transparent,
-          onPrimary: black,
-          primary: Colors.white,
-          brightness: Brightness.light,
-          onBackground: black,
-          onSurface: Colors.black,
-          secondary: black,
-          surface: Colors.grey) ,
-      scaffoldBackgroundColor: Colors.black,
-      appBarTheme: AppBarTheme(
-        iconTheme: IconThemeData(
-          color: Colors.black,
-        ),
+    primaryColor: Colors.white,
+    colorScheme: ColorScheme(onError: black,
+        onSecondary: black,
+        error: Colors.red,
+        background: Colors.transparent,
+        onPrimary: black,
+        primary: Colors.white,
+        brightness: Brightness.light,
+        onBackground: black,
+        onSurface: Colors.white,
+        secondary: Colors.white,
+        surface: Colors.grey),
+    scaffoldBackgroundColor: black,
+    appBarTheme: AppBarTheme(
+      iconTheme: IconThemeData(
         color: black,
       ),
-      textTheme: TextTheme(
-        headline1:TextStyle(
-          fontSize: 22,fontWeight: FontWeight.bold,color: black,
-        ),
-        subtitle1: TextStyle(fontSize: 14,fontWeight: FontWeight.bold,color: black
-        ),
-        subtitle2: TextStyle(fontSize: 14,fontWeight: FontWeight.bold,color: black
-        ),
+      color: black,
+    ),
+    textTheme: TextTheme(
+      headline1: TextStyle(
+        fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black,
       ),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor:Colors.transparent,
-        elevation: 0,
-        selectedItemColor: yallow,
-        unselectedItemColor: grey,
+      subtitle1: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.bold, color: Colors.black
       ),
+      subtitle2: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white
+      ),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      selectedItemColor: yallow,
+      unselectedItemColor: grey,
+    ),
   );
-
 }
